2fb5876d7e412d642c08da905fb432d0ccebe70a
leticia-amorim <l1e2t3i4@gmail.com>
2025-04-03 16:35:44 -0400

    terceiro commit

--- tarefa_1.js
+++ tarefa_1.js
@@ -1,6 +1,10 @@
 let valor = 13.141516;
 let text = "Meu primeiro projeto em javaScript";
 let bool = true;
-console.log(valor.toFixed(2))
-console.log(text.toLocaleUpperCase())
-console.log(bool.toString())
+
+console.log(valor.toFixed(2));
+console.log("Tipo: " + typeof valor);
+console.log(text.toLocaleUpperCase());
+console.log("Tipo: " + typeof text);
+console.log(bool.toString());
+console.log("Tipo: " + typeof ativo);
