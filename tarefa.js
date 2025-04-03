7f0d549274a2f28e0e4114eee79eb0dcfc3fe56d
leticia-amorim <l1e2t3i4@gmail.com>
2025-04-03 17:43:40 -0400

    primeiro commit

--- tarefa.js
+++ tarefa.js
@@ -1,2 +1,15 @@
-//primeiro Hello word
-console.log("helo, world");
+let nome = "leticia"
+let idade = 15
+console.log("Olá meu nome é" + nome + " e eu tenho" + idade + "anos");
+  
+
+let str = " sla sla"
+console.log(str.toLocaleLowerCase());
+console.log(str.toLocaleLowerCase());
+
+let numero = 8.4565
+console.log(typeof numero)
+
+let altura = 1.65
+let peso = 55
+console.log(( peso / (altura * altura)).toFixed(2));
--- tarefa_1.js
+++ tarefa_1.js
@@ -8,3 +8,25 @@ console.log(text.toLocaleUpperCase());
 console.log("Tipo: " + typeof text);
 console.log(bool.toString());
 console.log("Tipo: " + typeof ativo);
+
+let estados = ["são paulo", "minas Gerais", "Rondonia", "Acre"];
+let marcasCarros = [];
+marcasCarros.push("Toyota");
+console.log(estados[2]);
+console.log("tamanho" + estados.length);
+console.log(marcasCarros[0]);
+console.log("total letras: " + marcasCarros[0].length);
+console.log('Estados: ${estados[1]}')
+
+let usuario;
+console.log(usuario);
+
+let idade = 0;
+console.log (idade +1);
+
+let nula = null;
+//console.log(nula.length)
+console.log(nula + 1);
+
+const aula = "javascript";
+console.log(aula);
