f83cb5e80259ed49e11568ab45fff38be6a34fd5
leticia-amorim <l1e2t3i4@gmail.com>
2025-04-24 17:36:53 -0400

    COMMIT

---
+++ tarefa_2.js
@@ -0,0 +1,19 @@
+let numero1 = 6
+let numero2 = 4
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+
+let pessoa1 = 66;
+let pessoa2 = 50;
+let idade = pessoa1 > pessoa2 ? ("sim, a" + pessoa1 + "é mais velha") : ("não, a" + pessoa1 + "não ")
+console.log(idade)
+
+let idadePessoa = 38;
+let comparar = idadePessoa >= 18 ? "maior de idade" : "menor idade";
+console.log(comparar);
+
+let numero = 42
+let par = numero % 2 == 0 ? "é par" : "é impar";
+console.log(par);
